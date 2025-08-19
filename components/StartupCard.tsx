10fe6301a0131504216bc1e55f60b40ecbca0f0a
abhishek jaiswal <a.jaiswal@braininventory.com>
2025-08-19 16:42:50 +0530

    changes in the img classes

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -52,7 +52,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </div>
       <Link href={`/startup/${_id}`}>
         <p className="startup-card-desc line-clamp-3">{description}</p>
-        <img src={image} alt="placeholder" className="startup-card_img mt-3"/>
+        <img src={image} alt="placeholder" className="startup-card_img"/>
       </Link>
       <div className="flex-between gap-3 mt-5">
         <Link href={`?query=${category?.toLowerCase()}`}>
